1c03b5c96dcc9e02452aced71c7d8effdea3b36e
JoeGuidoboni <joe.guidoboni@gmail.com>
2023-12-19 22:53:29 -0500

    updated styling

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -6,48 +6,45 @@ export default function Gallery() {
   // const handleClose = () => {
   //   setOpen(false);
   // }
-  
+
   // const handleImage = (value) => {
   //   setImage(value);
   //   setOpen(true);
   // }
-     
+
   // const [open, setOpen] = useState(false);
   // const [image, setImage] = useState("false");
-
 
   return (
     <main className="bg-zinc-900 pb-32 font-mono">
       <NavBar></NavBar>
       <div className="w-full h-full flex items-center justify-center">
         <div className="max-w-5xl">
-          <div className="py-6">
+          <div className="border-amber-400 border-r-2 border-b-2 p-2 text-right">
             Some memories.
-            <br/>
-            I got into film photography some time in college and bring my Canon AE-1 with me any time I travel
+            <br />I got into film photography some time in college and bring my
+            Canon AE-1 with me any time I travel
           </div>
-          <ImageList variant="masonry" cols={3} gap={10}>
-            {Film.map((item) => (
-              <ImageListItem key={item.img}>
-                <img
-                  srcSet={`img/${item.img}`}
-                  src={`img/${item.img}`}
-                  alt={`img/${item.alt}`}
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
-          {/* <Modal
+          <div className="p-6">
+            <ImageList variant="masonry" cols={3} gap={10}>
+              {Film.map((item) => (
+                <ImageListItem key={item.img}>
+                  <img
+                    srcSet={`img/${item.img}`}
+                    src={`img/${item.img}`}
+                    alt={`img/${item.alt}`}
+                  />
+                </ImageListItem>
+              ))}
+            </ImageList>
+            {/* <Modal
             className={modalClasses}
             open={open} 
             >
           </Modal> */}
+          </div>
         </div>
       </div>
     </main>
   );
 }
-
-
-
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import NavBar from "@/components/navbar";
+import { Email, GitHub, Headphones, LinkedIn } from "@mui/icons-material";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,50 +8,73 @@ export default function Home() {
       <NavBar></NavBar>
       <div className="flex z-10 max-h-screen w-full justify-end p-8">
         <div className="basis-1/3">
-        <div className="flex-1 font-medium text-4xl tracking-tighter text-right p-4">
-          <div className="border-amber-400 border-r-2 border-b-2 p-2">
-            hey, i&apos;m jogu
+          <div className="flex-1 font-medium text-4xl tracking-tighter text-right p-4">
+            <div className="border-amber-400 border-r-2 border-b-2 p-2">
+              hey,
+              <br></br>
+              i&apos;m jogu
+            </div>
+
+            <div className="flex justify-end py-4">
+              <Image
+                src="/img/me.jpg"
+                width={300}
+                height={300}
+                alt="there's supposed to be a picture of me here"
+                className="object-cover rounded-lg"
+              />
+            </div>
           </div>
-          
-          <div className="flex justify-end py-4">
-            <Image
-              src="/img/me.jpg"
-              width={300}
-              height={300}
-              alt="there's supposed to be a picture of me here"
-              objectPosition="right"
-              className="object-cover rounded-lg"
-            />
-          </div>
-        </div>
         </div>
         <div className="basis-1/3 p-5 text-base">
           i&apos;m a software engineer with a passion for creating things that
-          help people, and that they enjoy using them. 
+          help people, and that they enjoy using them.
+          <br></br>
+          <br></br>i currently work at Fidelity Investments in Compensation Plan
+          Management where my team configures compensation plans for
+          Fidelity&apos;s 20,000+ financial reps.
           <br></br>
           <br></br>
-          i currently work at Fidelity Investments in Compensation Plan Management where my team configures compensation plans for Fidelity&apos;s 20,000+ financial reps.
-          <br></br>
-          <br></br>
-          in my free time i like to build things that i hope people will enjoy interacting with, like a{" "}
+          in my free time i like to build things that i hope people will enjoy
+          interacting with, like a{" "}
           <a
             className="underline decoration-amber-400"
             href="https://github.com/JoeGuidoboni/umami"
           >
             sarcastic dicord bot
-          </a>{"."}
+          </a>
+          {"."}
         </div>
         <div className="basis-1/3 p-4">
-          {/* <div className="">
-            contact
+          <div className="w-1/4 h-full border-amber-400 border-t-2 border-l-2 p-2">
+            <div className="text-xl">contact</div>
+            <ul>
+              <li className="py-1">
+                <a href="mailto:joe.guidoboni@gmail.com" className="underline decoration-amber-400">
+                  <Email className="pr-2" />
+                  email
+                </a>
+              </li>
+              <li className="py-1">
+                <a href="https://www.linkedin.com/in/joe-guidoboni/" className="underline decoration-amber-400">
+                  <LinkedIn className="pr-2" />
+                  linkedin
+                </a>
+              </li>
+              <li className="py-1">
+                <a href="https://github.com/JoeGuidoboni" className="underline decoration-amber-400">
+                  <GitHub className="pr-2" />
+                  github
+                </a>
+              </li>
+              <li className="py-1">
+                <a href="https://open.spotify.com/user/jofusgweedabownee" className="underline decoration-amber-400">
+                  <Headphones className="pr-2" />
+                  spotify
+                </a>
+              </li>
+            </ul>
           </div>
-          <ul>
-            <li>
-              <a>
-                email
-              </a>
-            </li>
-          </ul> */}
         </div>
       </div>
     </main>
